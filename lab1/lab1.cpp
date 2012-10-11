63de81f75b7709e638413c3d142ed8ee73deb793
tab <tab@tab-F5GL.(none)>
2012-10-11 19:03:20 +0200

    zaimplementowanie sredniej

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -4,6 +4,38 @@
 
 using namespace std;
 
+char separator = ',';
+
+void print(mpz_class x, int d)
+{
+	string number = x.get_str();
+	int length = number.length();
+
+
+	//jeśli precyzja jest większa lub równa długości liczby to trzeba dodać brakujące liczby z przodu
+	if (d >= length)
+		number.insert(0, d - length + 1, '0');
+
+	//dodanie przecinka
+	if (d != 0)
+		number.insert(number.length() - d, 1, separator);
+
+	string::iterator endOfString = number.end();
+	string::iterator it = endOfString;
+
+	//usuwamy niepotrzebne zera z tyłu i przecinki
+	//zawsze zostawiamy pierwszą liczbę
+	while (--it, ((*it == '0' || *it == separator ) && it != number.begin()));
+	
+	++it;
+	if (it != endOfString)
+	{
+		number.erase(it, endOfString);
+	}
+	cout << number << endl;
+
+}
+
 int main()
 {
 	mpz_class xi;
@@ -25,7 +57,6 @@ int main()
 	mpz_ui_pow_ui(temp, 10, d);
 	precision = mpz_class(temp);
 
-
 	while ((cin >> xi))
 	{
 		avg += xi;
@@ -35,9 +66,10 @@ int main()
 	}
 	
 
+	avg *= precision;
 	avg /= n;
-	cout << avg << endl;
 
+	print(avg, d);
 
 	return 0;
 }
