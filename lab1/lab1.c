fc03de3f344c448e0b65c21673a853225ec5a7d1
tab <tab@tab-F5GL.(none)>
2012-10-14 16:48:39 +0200

    wyglada na to że działa

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,41 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <gmp.h>
 
-
-/*using namespace std;
-
-char separator = '.';
-
-void print(mpz_class x, int d)
+void print(mpz_t x, int d)
 {
-	string number = x.get_str();
-	int length = number.length();
+	char* number = NULL;
+	number = mpz_get_str(number, 10, x);
+	int n = strlen(number);
+
+	char* zerosPtr = &(number[n-1]);
+
+	int zerosCount = 0;
+	int maxRemovedZeros = d;
 
 
-	//jeśli precyzja jest większa lub równa długości liczby to trzeba dodać brakujące liczby z przodu
-	if (d >= length)
-		number.insert(0, d - length + 1, '0');
-
-	//dodanie przecinka
-	if (d != 0)
-		number.insert(number.length() - d, 1, separator);
-
-	string::iterator endOfString = number.end();
-	string::iterator it = endOfString;
-
-	//usuwamy niepotrzebne zera z tyłu i przecinki
-	//zawsze zostawiamy pierwszą liczbę
-	while (--it, ((*it == '0' || *it == separator ) && it != number.begin()));
-	
-	++it;
-	if (it != endOfString)
+	//usunięcie niepotrzebnych zer z tyłu
+	//usuwamy maksymalnie d zer
+	//nie usuwamy pierwszej cyfry
+	//po tej operacji zostaje n-zerosCount liczb
+	while ((*zerosPtr == '0') && (zerosPtr != number) && (maxRemovedZeros--))
 	{
-		number.erase(it, endOfString);
+		--zerosPtr;
+		++zerosCount;
 	}
-	cout << number << endl;
+	//w tym miejscu kończymy napis
+	*(zerosPtr+1) = 0;
 
+
+	//jeżeli liczba nie posiada części ułamkowej
+	if ((d == zerosCount) || (number[0] == '0' && (strlen(number) == 1)))
+	{
+		printf("%s\n", number);
+	}
+	//jeżeli liczba nie posiada części całkowitej
+	else if (d >= n)
+	{
+		char* formatStr = (char*)malloc((2+d-n+zerosCount+3+1)*sizeof(char));
+		strcpy(formatStr, "0.");
+
+		memset(formatStr+2, '0', d-n+zerosCount);
+		*(formatStr+2+d-n+zerosCount) = 0;
+		strcat(formatStr, "%s\n");
+
+		printf(formatStr, number);
+	}
+	//jeżeli liczba posiada część całkowitą i ułamkową
+	else
+	{
+		//wydzielenie części całkowitej
+		char temp = number[n-d];
+		number[n-d] = 0;
+		//wypisanie części całkowitej
+		printf("%s.", number);
+
+		//wypisanie części ułamkowej
+		number[n-d] = temp;
+		printf("%s\n", number+n-d);
+	}
+	
 }
-*/
+
+mpz_t numbersRead[17000000];
 
 int main()
 {
@@ -52,36 +78,57 @@ int main()
 	int d;	
 
 	//liczba elementów na wejściu
-	int n = 0;
 
 	scanf("%d", &d);
 	mpz_ui_pow_ui(precision, 10, d);
 
-	while ((mpz_inp_str(x, stdin, 10)) != 0)
+	mpz_inp_str(x, stdin, 10);
+	mpz_add(avg, x, avg);
+	mpz_addmul(var, x, x);
+	mpz_set(numbersRead[0], x);
+
+	int n = 1;
+	int periodLength = 1;
+	int periodIndex = 0;
+
+	while (mpz_inp_str(x, stdin, 10) != 0)
 	{
 		mpz_add(avg, x, avg);
 		mpz_addmul(var, x, x);
-//		periodPattern.push_back(xi);
+		mpz_set(numbersRead[n], x);
 		++n;
+
+		if (mpz_cmp(numbersRead[periodIndex % periodLength], x))
+		{
+			periodLength += periodIndex + 1;
+			periodIndex = 0;
+		}
+		else
+			++periodIndex;
+
 	}
 	
+	mpz_t avgSqr;
+	mpz_init(avgSqr);
+	mpz_mul(avgSqr, avg, avg);
+
 	mpz_mul_ui(var, var, n);
-	mpz_t temp;
-	mpz_init(temp);
-	mpz_mul(temp, avg, avg);
-	mpz_sub(var, var, temp);
+	mpz_sub(var, var, avgSqr);
 	mpz_mul(var, var, precision);
-	mpz_set_ui(temp, 1);
-	mpz_mul_ui(temp, temp, n*n);
-	mpz_div(var, var, temp);
+
+	mpz_t nSqr;
+	mpz_init(nSqr);
+	mpz_set_ui(nSqr, 1);
+	mpz_mul_ui(nSqr, nSqr, n*n);
+
+	mpz_div(var, var, nSqr);
 
 	mpz_mul(avg, avg, precision);
 	mpz_div_ui(avg, avg, n);
 
-	//print(avg, d);
-	
-	//print(var, d);
-
+	print(avg, d);
+	print(var, d);
+	printf("%d\n", periodLength);
 
 	return 0;
 }
